9d5f6434c40b2c9dc71d3489f75726545d79e839
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-12 23:11:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
 
 export default App;
