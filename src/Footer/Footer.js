a37854a668e9042c3f4dc0995e8b5a111c912956
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-12 23:13:54 +0530

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Footer.css";
 import FooterContainer1 from "./FooterContainer1/FooterContainer1";
 import FooterContainer2 from "./FooterContainer2/FooterContainer2";
 import FooterContainer3 from "./FooterContainer3/FooterContainer3";
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <FooterContainer1 />
-        <FooterContainer2 />
-        <FooterContainer3 />
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div className="footer">
+    <FooterContainer1 />
+    <FooterContainer2 />
+    <FooterContainer3 />
+  </div>
+);
 
 export default Footer;
