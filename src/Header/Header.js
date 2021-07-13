b626cb44e536035e72ff31e6698e71067b533ae8
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-13 19:27:21 +0530

    Update Header.js

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import TopLayout from "./TopLayout";
 import Nav from "./Nav";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <TopLayout />
-        <Nav />
-      </div>
-    );
-  }
-}
+const Header = () => (
+  <div className="header">
+    <TopLayout />
+    <Nav />
+  </div>
+);
 
 export default Header;
