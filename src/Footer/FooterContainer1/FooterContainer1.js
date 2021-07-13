8506e36620256796ef84d77275fa4b17dc7cebf6
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-13 14:40:50 +0530

    Update FooterContainer1.js

--- src/Footer/FooterContainer1/FooterContainer1.js
+++ src/Footer/FooterContainer1/FooterContainer1.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./FooterCont1.css";
 import GridItem1 from "./GridItem1";
-import GridItem2 from "./GridItem2";
 import GridItem3 from "./GridItem3";
 
 const FooterContainer1 = () => (
   <div className="footerContainer1">
     <GridItem1 />
-    <GridItem2 />
     <GridItem3 />
   </div>
 );
 
 export default FooterContainer1;
-
